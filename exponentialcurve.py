70819ffae8758d314c9231bd3bda02a9da422ec5
victorlidskog <38893767+victorlidskog@users.noreply.github.com>
2020-03-04 16:55:22 +0100

    Update exponentialcurve.py

--- exponentialcurve.py
+++ exponentialcurve.py
@@ -6,36 +6,57 @@ def sum_list(l):
     for x in l:
         sum += x
     return sum
+def sumsquare(x):
+    total = 0
+    for ele in range(0, len(x)): 
+        total = total + x[ele]*x[ele]
+    return total
 
+def sumln(x):
+    total = 0
+    for ele in range(0, len(x)): 
+        total = total + math.log(x[ele])
+    return total
+
+def sumlnxi(x,y):
+    total = 0
+    for ele in range(0, len(x)): 
+        total = total + (math.log(x[ele]))*(y[ele])
+    return total
+
+def subroom(x):
+    total = 0
+    for ele in range(0, len(x)): 
+        total = total + (math.log(x[ele]))*(y[ele])
+    return total
 
 with open("lab4.txt", "r") as fo:
-    time = fo.read().split()[0::2]
+    time = fo.read().split()[0::2] # What does this line of code mean?? is it read column 0 to the end every 2 entries?
     for i in range(0, len(time)): 
-        time[i] = int(time[i]) 
-    #print(time)
-    print(len(time))
-with open("lab4.txt", "r") as fo:
-    temperature = fo.read().split()[1::2]
-    temp = temperature.replace(',','.')
-    print(temperature)
-    #temperature = np.array(temperature).astype(np.float)
-    #for i in range(0, len(temperature)): 
-     #   time[i] = float(temperature[i]) 
-    print(len(temperature))
-    #print(temperature)
-    #print(temperature[0])
+        time[i] = float(time[i]) 
 
-#print(sum_list(time))
-r = sum_list(time)
-#print(temperature[2])
-A = np.array([[r, 0],[0, 0]])
-print(A[0,0])
-#B = np.array([0 , 0])
-#print(B[0,:])
-#Ainv = np.linalg.inv(A)
-#print("this is inverse of A", Ainv)
-#C = np.matmul(Ainv,B)
-#print("This is Ainv*B",C)
-#d = C[0]
-#D = math.exp(d)
-#print("This is e to the mult",D)
+with open("lab4.txt", "r") as fo:
+    temperature = fo.read().split()[1::2] # What does this line of code mean?? is it read column 1 to the end every 2 entries?
+    
+    temp = np.ndarray((len(temperature),),float)
+    for i in range(0, len(temperature)):
+        temp[i] = temperature[i].replace(',', '.')
+        temp[i] = float(temp[i])
+
+for x in range(0, len(temperature)):
+    temp[x] -= 30.68674
+
+r = len(time)
+xi = sum_list(time)
+xisq = sumsquare(time)
+lnyi = sumln(temp)
+xilnyi = sumlnxi(temp,time)
+A = np.array([[r, xi],[xi, xisq]])
+Ainv = np.linalg.inv(A)
+B = np.array([lnyi , xilnyi])
+C = np.matmul(Ainv,B)
+d = C[0]
+D = math.exp(d)
+print("The Startvalue is :",D)
+print("The timeconstant for pt100 is :",C[1])
+print("The exponential funiton is :f(x)=",D,"e^",C[1],"t + 30.68674")
